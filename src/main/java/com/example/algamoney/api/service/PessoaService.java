736ead7513f2385ed32d4d1ebbf6ae8d8d10c6c3
Anderson Lopes <anderson.lopes.2010@gmail.com>
2017-08-02 22:22:54 -0300

    Atualização paracial de propriedade com PUT.

--- src/main/java/com/example/algamoney/api/service/PessoaService.java
+++ src/main/java/com/example/algamoney/api/service/PessoaService.java
@@ -15,13 +15,24 @@ public class PessoaService {
 	private PessoaRepository pessoaRepository;
 
 	public Pessoa atualizar(Long id, Pessoa pessoa) {
+		Pessoa pessoaSalva = buscarPeloId(id);
+
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "id");
+		return pessoaRepository.save(pessoaSalva);
+	}
+
+	public Pessoa atualizarPropriedadeAtivo(Long id, Boolean ativo) {
+		Pessoa pessoaSalva = buscarPeloId(id);
+		pessoaSalva.setAtivo(ativo);
+		return pessoaRepository.save(pessoaSalva);
+	}
+
+	private Pessoa buscarPeloId(Long id) {
 		Pessoa pessoaSalva = pessoaRepository.findOne(id);
 		if (pessoaSalva == null) {
 			throw new EmptyResultDataAccessException(1);
 		}
-
-		BeanUtils.copyProperties(pessoa, pessoaSalva, "id");
-		return pessoaRepository.save(pessoaSalva);
+		return pessoaSalva;
 	}
 
 }
