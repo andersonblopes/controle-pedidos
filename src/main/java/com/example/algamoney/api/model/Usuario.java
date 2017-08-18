00c1a879577efea03e4bc4b93f55b0e003ff3275
Anderson Lopes <anderson.lopes.2010@gmail.com>
2017-08-17 22:17:07 -0300

    Nomo do usuário no token JWT.

--- src/main/java/com/example/algamoney/api/model/Usuario.java
+++ src/main/java/com/example/algamoney/api/model/Usuario.java
@@ -18,9 +18,7 @@ public class Usuario {
 	private Long id;
 
 	private String nome;
-
 	private String email;
-
 	private String senha;
 
 	@ManyToMany(fetch = FetchType.EAGER)
--- src/main/java/com/example/algamoney/api/security/AppUserDetailsService.java
+++ src/main/java/com/example/algamoney/api/security/AppUserDetailsService.java
@@ -8,7 +8,6 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -26,15 +25,13 @@ public class AppUserDetailsService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		Optional<Usuario> usuarioOptional = usuarioRepository.findByEmail(email);
-		Usuario usuario = usuarioOptional
-				.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha inválidos!"));
-		return new User(email, usuario.getSenha(), getPermissoes(usuario));
+		Usuario usuario = usuarioOptional	.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha inválidos!"));
+		return new UsuarioSistema(usuario, getPermissoes(usuario));
 	}
 
 	private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
 		Set<SimpleGrantedAuthority> authorites = new HashSet<>();
-		usuario.getPermissoes()
-				.forEach(p -> authorites.add(new SimpleGrantedAuthority(p.getDescricao().toUpperCase())));
+		usuario.getPermissoes().forEach(p -> authorites.add(new SimpleGrantedAuthority(p.getDescricao().toUpperCase())));
 		return authorites;
 	}
 
